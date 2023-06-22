0c84580d0ff0aeeddc16f8a2ca68296029a5512d
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-22 11:37:31 +0200

    scaffolding refactoring

---
+++ src/components/AppBody.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <AppSearchbar/>        
+    </header>
+
+    <main>
+        <AppMain/>
+    </main>
+</template>
+
+<script>
+import AppSearchbar from './AppSearchbar.vue';
+import AppMain from './AppMain.vue';
+
+export default {
+    name: "AppBody",
+
+    components: {
+        AppSearchbar,
+        AppMain
+    },
+
+    methods: {
+        getMovie(){
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=388bc42a36d295b0d1d3ccd86ca4779e&query=' + store.searchText)
+            .then((response) => {
+            console.log(response.data.results);
+            this.items = response.data.results;
+            console.log(this.items);
+            })
+            .catch(function (error) {
+            console.log(error);
+             })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -23,18 +23,6 @@ export default {
     components:{
         ItemsList,
     },
-
-    updated(){
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=388bc42a36d295b0d1d3ccd86ca4779e&query=' + store.searchText)
-            .then((response) => {
-            console.log(response.data.results);
-            this.items = response.data.results;
-            console.log(this.items);
-            })
-        .catch(function (error) {
-            console.log(error);
-        })
-    },
 }
 </script>
 
