ba29651cf1635282576f835733ef2c2295d97647
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-22 16:20:52 +0200

    creating get query to have tv series rendered as well

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -41,6 +41,18 @@ export default {
             .catch(function (error) {
             console.log(error);
              })
+        },
+
+        getTvSeries(){
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=388bc42a36d295b0d1d3ccd86ca4779e&query=' + store.searchText)
+            .then((response) => {
+            console.log(response.data.results);
+            this.items = response.data.results;
+            console.log(this.items);
+            })
+            .catch(function (error) {
+            console.log(error);
+             })
         }
     }
 }
